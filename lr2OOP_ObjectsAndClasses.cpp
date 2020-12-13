5bdbcf9110436c62de8f1f8d8462a0ac9e002b77
gedovirhir <nikita4122@mail.ru>
2020-12-14 01:52:09 +0500

    Update lr2OOP_ObjectsAndClasses.cpp

--- lr2OOP_ObjectsAndClasses.cpp
+++ lr2OOP_ObjectsAndClasses.cpp
@@ -1,72 +1,106 @@
 #include <iostream>
 #include <cstdlib>
 #include <conio.h>
-
-
+#include <Windows.h>
 using namespace std;
 
-class Point {
+class Point3D {
     public:
-        int x, y;
-
-        Point() {
-            x = 0;
-            y = 0;
-        }
-        Point(int x, int y) {
-            this->x = x;
-            this->y = y;
-        }
-        Point(Point &p) {
-            x = p.x;
-            y = p.y;
-        }
-        Point(Point *p) {
-            x = p->x;
-            y = p->y;
-        }
-};
-
-class Point3D : Point {
-    public:
-        int z;
-
-        Point3D() {
-            x = 0;
-            y = 0;
-            z = 0;
-        }
-        Point3D(int x, int y, int z) : Point(x,y) {
-            this->z = z;
-        }
-        Point3D(Point &p, int z) : Point(p) {
-            this->z = z;
-        }
-        Point3D(Point *p, int z) : Point(p) {
-            this->z = z;
-        }
-        Point3D(Point &p) {
-            x = p.x;
-            y = p.y;
-        }
-        Point3D(Point *p) {
-            x = p->x;
-            y = p->y;
-        }
+        int xcord, ycord, zcord;
 };
 
 class Figure {
     protected:
-        int xcord;
-        int ycord;
-        
+        int x, y;
+
         Figure() {
-            xcord = 0;
-            ycord = 0;
+            x = 0;
+            y = 0;
+        }
+        Figure(int x, int y) {
+            this->x = x;
+            this->y = y;
+        }
+        Figure(Figure &p) {
+            x = p.x;
+            y = p.y;
+        }
+        Figure(Figure *p) {
+            x = p->x;
+            y = p->y;
         }
 
-        Figure(int x, int y) {
-            xcord = x;
-            ycord = y;
+        virtual ~Figure() {
+            printf("Figure::~Figure()\n");
+        }
+    public:
+        void set(int x, int y) {
+            this->x = x;
+            this->y = y;
+        }
+        void getCords() {
+            cout << "X: " << x << " Y: " << y;
         }
 };
+
+class Figure3D : protected Figure {
+    protected:
+        int z;
+
+        Figure3D() {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+        Figure3D(int x, int y, int z) : Figure(x,y) {
+            this->z = z;
+        }
+        Figure3D(Figure &p, int z) : Figure(p) {
+            this->z = z;
+        }
+        Figure3D(Figure *p, int z) : Figure(p) {
+            this->z = z;
+        }
+        Figure3D(Figure3D &p) {
+            x = p.x;
+            y = p.y;
+        }
+        Figure3D(Figure3D *p) {
+            x = p->x;
+            y = p->y;
+        }
+
+        ~Figure3D() {
+            printf("Figure3D::~Figure3D()\n");
+        }
+};
+
+class Ellipse : public Figure {
+    private:
+        int osx;
+        int osy;
+    public:
+        Ellipse() {
+            osx = 1;
+            osy = 1;
+        }
+        Ellipse(int x, int y, int osx, int osy) : Figure(x,y) {
+            this->osx = osx;
+            this->osy = osy;
+        }
+        Ellipse(Ellipse &e) {
+            osx = e.osx;
+            osy = e.osy;
+        }
+        Ellipse(Ellipse *e) {
+            osx = e->osx;
+            osy = e->osx;
+        }
+};
+
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+}
